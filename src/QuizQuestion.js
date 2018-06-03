75db5a0497085124dfae440a50ed712515e038a6
vigneshwarr1990 <vigneshwarr1990@gmail.com>
2018-06-03 12:32:26 -0500

    completed module 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = { incorrectAnser:false }
+    }
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
+            this.setState({incorrectAnser : false})
             this.props.showNextQuestionHandler()
+        }else{
+            this.setState({incorrectAnser : true})
         }
     }
 
@@ -22,6 +30,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
+                {this.state.incorrectAnser ? <p className='error'> Sorry, that's not right</p> : null}
             </main>
         )
     }
