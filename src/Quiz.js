db4ae49650c0f607b7d60d1dae8e440fe5415d69
vigneshwarr1990 <vigneshwarr1990@gmail.com>
2018-06-02 20:44:29 -0500

    fixed Module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,9 +16,9 @@ class Quiz extends Component {
 
         return (
             <div>
-                {isQuizEnd} ?
+                {isQuizEnd ?
                     <QuizEnd />
-                : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                    : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
 
             </div>
         )
